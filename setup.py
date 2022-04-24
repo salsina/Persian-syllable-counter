0dbb2b1e34c8db4fcedd4ad8e1f9744b7546adde
unknown <shaghayeghladani@yahoo.com>
2022-04-25 03:08:43 +0430

    Add setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='persian_sy',
+    name='persian_syllable_counter',
     version='1.0.0',
     description='This library can count the syllables of persian text or words by converting the persian text to finglish format',
     long_description=readme,
-    author='Nb',
+    author='Nbic',
     long_description_content_type="text/markdown",
-    url="https://github.com/shaqayeql/syllable/",
+    url="https://github.com/salsina/Persian-syllable-counter/",
     packages=find_packages(),
 )
