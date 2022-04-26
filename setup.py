91c1af05991acdf87bd9390eb63dbd42ddda254c
unknown <shaghayeghladani@yahoo.com>
2022-04-27 00:27:35 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='persian_syllable',
-    version='1.0.0',
+    name='persiansyllablecount',
+    version='1.0.3',
     description='This library can count the syllables of persian text or words by converting the persian text to finglish format',
     long_description=readme,
     author='Nbic',
     long_description_content_type="text/markdown",
-    url="https://github.com/salsina/Persian-syllable-counter/",
-    packages=find_packages(),
+    packages=find_packages(include=["persiansyllablecount*"]),
+    install_requires=[],
+
+    keywords=['python', 'first package'],
+        classifiers= [
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ]
 )
